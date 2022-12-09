969f2d0ce1efe5db5f90f8abe71ecc343f9015a7
Masaba Musa <masamata98@gmail.com>
2022-12-09 11:35:37 +0300

    Add descriptive info to my readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 ## TO-DO LIST PROJECT
 --------
+This is JS-CAPSTONE project that was aimed at testing my vanilla javascript skills. It was a good one since it gave me a fair duel with some serious javascript concepts like :-
+
+* functions
+* DOM
+* arrays
+* objects
+* localStorage etc.
+
+### STEPS I FOLLOWED
+I always start by creating a Kanban board for my project which gives me a clear insight of the tasks ahead and helps gain an understanding of the chores, so that I focus on them first.
+
+Secondly, I brain stormed and created a wireframe using Figma which was quiet easy to come up with and this greatly helped me to gain a clear view of what I wanted my users to interact with.
+
+Thirdly, I now started putting writing my vision into code.
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,58 +1,58 @@
-window.onload=function loadTodos() {
-    loadTodosFromLocalStorage()
-}
-const date=document.querySelector("#date")
-const btn=document.querySelector(".btn")
-const lists=document.querySelector("#lists")
-const input=document.querySelector("#text")
-const form = document.querySelector("#form")
-const edit =document.querySelector('.fa-pen-to-square')
-const alert=document.querySelector('.alert')
-let listItem=document.querySelector('#lists').childNodes;
+window.onload = function loadTodos() {
+  loadTodosFromLocalStorage();
+};
+const date = document.querySelector("#date");
+const btn = document.querySelector(".btn");
+const lists = document.querySelector("#lists");
+const input = document.querySelector("#text");
+const form = document.querySelector("#form");
+const edit = document.querySelector(".fa-pen-to-square");
+const alert = document.querySelector(".alert");
+let listItem = document.querySelector("#lists").childNodes;
 
 //make the todo value to edit global
-let currentEditValue=""
+let currentEditValue = "";
 //array of todos
-let todos=[]
+let todos = [];
 //function that creates list element
-let listElement=(todo_task,checker=false,textDecoration="none")=>{
-    let task=document.createElement('li');
-    let label=document.createElement('label');
-    let checkmark=document.createElement('span');
-    let check=document.createElement('input');
-    let cover=document.createElement('span')
-    let upperDiv=document.createElement('div')
-    let icons=document.createElement('div')
-    check.setAttribute('type','checkbox')
-    check.setAttribute('checked',checker)
-    let editInput=document.createElement('input')
-    editInput.setAttribute('type', 'text');
-    editInput.className = "editInput";
-    upperDiv.className="check-text"
-    label.className="container"
-    checkmark.className="checkmark"
-    cover.className="item-text"
-    cover.style.textDecoration=textDecoration
-    check.className="custom-checkbox"
-    let button=document.createElement('button')
-    check.checked=checker
-    button.className = "editBtn";
-    button.innerHTML="Edit"
-    icons.innerHTML='<i class="fa-regular fa-pen-to-square"></i>'
-    icons.innerHTML+='<i class="fa-solid fa-trash-can"></i>'
-    task.classList.add('list-item')
-    lists.appendChild(task);
-    label.appendChild(check)
-    label.appendChild(checkmark)
-    upperDiv.appendChild(label)
-    upperDiv.appendChild(cover)
-    cover.textContent=todo_task;
-    task.appendChild(upperDiv)
-    task.appendChild(editInput)
-    task.appendChild(button)
-    task.appendChild(icons);
-    input.value=""
-}
+let listElement = (todo_task, checker = false, textDecoration = "none") => {
+  let task = document.createElement("li");
+  let label = document.createElement("label");
+  let checkmark = document.createElement("span");
+  let check = document.createElement("input");
+  let cover = document.createElement("span");
+  let upperDiv = document.createElement("div");
+  let icons = document.createElement("div");
+  check.setAttribute("type", "checkbox");
+  check.setAttribute("checked", checker);
+  let editInput = document.createElement("input");
+  editInput.setAttribute("type", "text");
+  editInput.className = "editInput";
+  upperDiv.className = "check-text";
+  label.className = "container";
+  checkmark.className = "checkmark";
+  cover.className = "item-text";
+  cover.style.textDecoration = textDecoration;
+  check.className = "custom-checkbox";
+  let button = document.createElement("button");
+  check.checked = checker;
+  button.className = "editBtn";
+  button.innerHTML = "Edit";
+  icons.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
+  icons.innerHTML += '<i class="fa-solid fa-trash-can"></i>';
+  task.classList.add("list-item");
+  lists.appendChild(task);
+  label.appendChild(check);
+  label.appendChild(checkmark);
+  upperDiv.appendChild(label);
+  upperDiv.appendChild(cover);
+  cover.textContent = todo_task;
+  task.appendChild(upperDiv);
+  task.appendChild(editInput);
+  task.appendChild(button);
+  task.appendChild(icons);
+  input.value = "";
+};
 
 //button functionality
 const add = () => {
@@ -64,7 +64,7 @@ const add = () => {
       alert.innerHTML = "Add Task";
       alert.style.display = "block";
     } else {
-      //conver input value to sentence case
+      //convert input value to sentence case
       (function () {
         let stringToConvert = input.value;
         stringToConvert = stringToConvert
@@ -74,36 +74,36 @@ const add = () => {
           todos.push({ text: stringToConvert, checked: false });
           let task = todos[todos.length - 1];
           localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
-          listElement(task.text,false);
+          listElement(task.text, false);
         } else {
-            let arrayOfText=[]
-            todos.forEach(element => {
-               arrayOfText.push(element.text) 
-            });
-            if (!arrayOfText.includes(stringToConvert)) {
-              todos.push({ text: stringToConvert, checked: false });
-              let task = todos[todos.length - 1];
-              listElement(task.text,false);
-              if (todos.length > 0) {
-                if (localStorage.length <= 0) {
-                  localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
-                } else {
-                  let addToLocalStorage = todos[todos.length - 1];
-                  let currentStorageItems = JSON.parse(
-                    localStorage.getItem("Mon")
-                  );
-                  let arrayOfTodos = currentStorageItems.todos;
-                  let newTodosArray = arrayOfTodos.concat(addToLocalStorage);
-                  localStorage.setItem(
-                    "Mon",
-                    JSON.stringify({ todos: newTodosArray })
-                  );
-                }
+          let arrayOfText = [];
+          todos.forEach((element) => {
+            arrayOfText.push(element.text);
+          });
+          if (!arrayOfText.includes(stringToConvert)) {
+            todos.push({ text: stringToConvert, checked: false });
+            let task = todos[todos.length - 1];
+            listElement(task.text, false);
+            if (todos.length > 0) {
+              if (localStorage.length <= 0) {
+                localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
+              } else {
+                let addToLocalStorage = todos[todos.length - 1];
+                let currentStorageItems = JSON.parse(
+                  localStorage.getItem("Mon")
+                );
+                let arrayOfTodos = currentStorageItems.todos;
+                let newTodosArray = arrayOfTodos.concat(addToLocalStorage);
+                localStorage.setItem(
+                  "Mon",
+                  JSON.stringify({ todos: newTodosArray })
+                );
               }
-            } else {
-              alert.innerHTML = "Task already added";
-              alert.style.display = "block";
             }
+          } else {
+            alert.innerHTML = "Task already added";
+            alert.style.display = "block";
+          }
         }
       })();
     }
@@ -117,144 +117,132 @@ const loadTodosFromLocalStorage = () => {
     todos = arrayOfTodos;
     if (todos.length) {
       todos.forEach((element) => {
-        listElement(element.text,element.checked,element.checked?"line-through":"none");
+        listElement(
+          element.text,
+          element.checked,
+          element.checked ? "line-through" : "none"
+        );
       });
     }
   }
 };
 
 //edit todo
-lists.addEventListener('click',(e)=>{
-    if (e.target.classList.contains("fa-pen-to-square")) {
-        const text=e.target.parentElement.parentElement.childNodes[1]
-        let editInput=e.target.parentElement.parentElement.childNodes[1]
-        let button=e.target.parentElement.parentElement.childNodes[2]
-        editInput.style.display="block";
-        button.style.display="block"
-        editInput.value=e.target.parentElement.parentElement.childNodes[0].textContent
-        e.target.parentElement.parentElement.firstChild.lastChild.textContent=""
-        currentEditValue=text.value
-    }
-})
-lists.addEventListener('click',(e)=>{
-    if (e.target.classList.contains("editBtn")){
-        let editInput=e.target.parentElement.childNodes[1]
-        let button=e.target.parentElement.childNodes[2]
-        if (editInput.value!=="") {
-           let value=editInput.value;
-           value=value
-           .toLowerCase()
-           .replace(/(^|\s)\S/, (L) => L.toUpperCase());
-           e.target.parentElement.childNodes[0].childNodes[1].textContent=value;
-           editInput.style.display="none";
-           button.style.display="none"
-           if (currentEditValue!==value) {
-            e.target.parentElement.childNodes[0].childNodes[0].childNodes[0].checked=false
-            e.target.parentElement.childNodes[0].childNodes[1].style.textDecoration="none"
-            let index=todos.findIndex(item=>item.text===currentEditValue)
-            todos.splice(index,1,{text:value,checked:false})
-            localStorage.setItem(
-                    "Mon",
-                    JSON.stringify({ todos: todos })
-                  );
-            // let addToLocalStorage = todos[index];
-            // console.log(add)
-            // let currentStorageItems = JSON.parse(
-            //         localStorage.getItem("Mon")
-            //       );
-            // let arrayOfTodos = currentStorageItems.todos;
-            // let newTodosArray = arrayOfTodos.splice(index,1,addToLocalStorage);      
-                  
-           }
-           
-        }   
-    }
-})
-//remove todo
-lists.addEventListener('click',(e)=>{
-    if (e.target.classList.contains("fa-trash-can")) {
-        let text=e.target.parentElement.parentElement.innerText
-        let index=todos.findIndex(item=>item.text===text);
-        todos.splice(index,1)
-        localStorage.setItem('Mon',JSON.stringify({todos:todos}))
-        e.target.parentElement.parentElement.remove()
-    }
-})
-//completed tasks
-lists.addEventListener('click',(e)=>{
-    if (e.target.classList.contains("custom-checkbox")) {
-        //checkbox state
-        let checkBoxState=e.target.checked;
-        console.log(checkBoxState)
-        let text=e.target.parentElement.parentElement.childNodes[1];
-        let todoText=text.textContent
-        if (checkBoxState===true) { 
-            text.style.textDecoration = "line-through";
-            todos.forEach(element=> {
-              if (element.text === todoText) {
-                element.checked = true;
-                localStorage.setItem(
-                  "Mon",
-                  JSON.stringify({ todos: todos})
-                );
-              }
-            });
-        }else{
-            text.style.textDecoration="none"
-            todos.forEach(element=> {
-                if (element.text === todoText) {
-                  element.checked = false;
-                  localStorage.setItem(
-                    "Mon",
-                    JSON.stringify({ todos: todos})
-                  );
-                }
-              });
-        }
-    }
-})
-//Date functionality
-const newDate=()=>{
-   let today=new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    let dayId =today.getDay()
-    switch (dayId) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-          day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
+lists.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-pen-to-square")) {
+    const text = e.target.parentElement.parentElement.childNodes[1];
+    let editInput = e.target.parentElement.parentElement.childNodes[1];
+    let button = e.target.parentElement.parentElement.childNodes[2];
+    editInput.style.display = "block";
+    button.style.display = "block";
+    editInput.value =
+      e.target.parentElement.parentElement.childNodes[0].textContent;
+    e.target.parentElement.parentElement.firstChild.lastChild.textContent = "";
+    currentEditValue = text.value;
+  }
+});
+lists.addEventListener("click", (e) => {
+  if (e.target.classList.contains("editBtn")) {
+    let editInput = e.target.parentElement.childNodes[1];
+    let button = e.target.parentElement.childNodes[2];
+    if (editInput.value !== "") {
+      let value = editInput.value;
+      value = value.toLowerCase().replace(/(^|\s)\S/, (L) => L.toUpperCase());
+      e.target.parentElement.childNodes[0].childNodes[1].textContent = value;
+      editInput.style.display = "none";
+      button.style.display = "none";
+      if (currentEditValue !== value) {
+        e.target.parentElement.childNodes[0].childNodes[0].childNodes[0].checked = false;
+        e.target.parentElement.childNodes[0].childNodes[1].style.textDecoration =
+          "none";
+        let index = todos.findIndex((item) => item.text === currentEditValue);
+        todos.splice(index, 1, { text: value, checked: false });
+        localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
       }
-
-today = `${dd}/${mm}/${yyyy}-${day.slice(0,3)}`;
-date.innerHTML=today; 
-}
-newDate()
-add()
-/// Change functionality of Enter Key
-input.addEventListener('keyup', (e)=>{
-    if (e.keyCode === 13) {
-        document.querySelector(".btn").click() 
     }
-})
+  }
+});
+//remove todo
+lists.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-trash-can")) {
+    let text = e.target.parentElement.parentElement.innerText;
+    let index = todos.findIndex((item) => item.text === text);
+    todos.splice(index, 1);
+    localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
+    e.target.parentElement.parentElement.remove();
+  }
+});
+//completed tasks
+lists.addEventListener("click", (e) => {
+  if (e.target.classList.contains("custom-checkbox")) {
+    //checkbox state
+    let checkBoxState = e.target.checked;
+    console.log(checkBoxState);
+    let text = e.target.parentElement.parentElement.childNodes[1];
+    let todoText = text.textContent;
+    if (checkBoxState === true) {
+      text.style.textDecoration = "line-through";
+      todos.forEach((element) => {
+        if (element.text === todoText) {
+          element.checked = true;
+          localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
+        }
+      });
+    } else {
+      text.style.textDecoration = "none";
+      todos.forEach((element) => {
+        if (element.text === todoText) {
+          element.checked = false;
+          localStorage.setItem("Mon", JSON.stringify({ todos: todos }));
+        }
+      });
+    }
+  }
+});
+//Date functionality
+const newDate = () => {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = today.getFullYear();
+  let dayId = today.getDay();
+  switch (dayId) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+  today = `${dd}/${mm}/${yyyy}-${day.slice(0, 3)}`;
+  date.innerHTML = today;
+};
+newDate();
+add();
+/// Change functionality of Enter Key
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    document.querySelector(".btn").click();
+  }
+});
 /// prevent the form default command/refresh
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+let numberOfEggs=30||0;
